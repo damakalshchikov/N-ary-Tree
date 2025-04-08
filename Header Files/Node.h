156b3fb2935fbb98c93af0961b4257e8d531db69
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-09 00:37:46 +0300

    Добавлена документация к функциям

--- Header Files/Node.h
+++ Header Files/Node.h
@@ -9,19 +9,45 @@
 
 using namespace std;
 
+/**
+ * Класс, представляющий узел n-арного дерева
+ */
 class Node {
 public:
+    /**
+     * Конструктор класса Node
+     * @param number - числовое значение узла
+     * @param arity - арность узла
+     */
     Node(const int& number, const int& arity);
 
+    /**
+     * Получить числовое значение узла
+     * @return Числовое значение узла
+     */
     int getNumber() const;
 
+    /**
+     * Получить арность узла
+     * @return Арность узла
+     */
     int getArity() const;
 
+    /**
+     * Получить всех потомков узла
+     * @return Вектор потомков узла
+     */
     vector<Node*>& getChildren();
 
+    /**
+     * Получить потомка по индексу
+     * @param number - индекс потомка
+     * @return Потомок узла
+     */
     Node* getChild(const int& number) const;
 
 private:
+    // Числовое значение, арность и потомки узла
     int number;
     int arity;
     vector<Node*> children;
--- Source Files/Node.cpp
+++ Source Files/Node.cpp
@@ -7,23 +7,45 @@
 
 using namespace std;
 
+/**
+ * Конструктор класса Node
+ * @param number - числовое значение узла
+ * @param arity - арность узла
+ */
 Node::Node(const int &number, const int &arity) : number(number), arity(arity) {
     // Инициализируем вектор размером arity потомков, где по умолчанию каждый потомок - nullptr
     children.resize(arity, nullptr);
 }
 
+/**
+ * Получить числовое значение узла
+ * @return Числовое значение узла
+ */
 int Node::getNumber() const {
     return this->number;
 }
 
+/**
+ * Получить арность узла
+ * @return Арность узла
+ */
 int Node::getArity() const {
     return this->arity;
 }
 
+/**
+ * Получить всех потомков узла
+ * @return Вектор потомков узла
+ */
 vector<Node*>& Node::getChildren() {
     return this->children;
 }
 
+/**
+ * Получить потомка по индексу
+ * @param number - индекс потомка
+ * @return Потомок узла
+ */
 Node* Node::getChild(const int& number) const {
     return this->children[number];
 }

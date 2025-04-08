ceed7d587e6f1b2932f62b623c4b95b1f561ccf3
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-09 00:38:45 +0300

    Тестирование чтения из файла

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "../Header Files/Node.h"
+#include "../Header Files/FileReader.h"
+
 using namespace std;
 
 int main(const int argc, char *argv[]) {
@@ -10,29 +13,22 @@ int main(const int argc, char *argv[]) {
     if (argc < 2) {
         wcout << L"Аргументы, которые можно передать программе:" << endl;
         wcout << L"programMode - режим работы программы ";
-        wcout << L"(1 - чтение существующего файла, 2 - генерация нового файла и его последующее чтение)" << endl;
+        wcout << L"(1 - чтение существующего файла, 2 - генерация нового файла и его чтение)" << endl;
         wcout << L"fileName - имя файла для чтения" << endl;
-        return 0;
+        wcout << L"arity - арность дерева" << endl;
+        return 1;
     }
 
     // progMode - режим работы программы
     // fileName - имя файла
     const int programMode = atoi(argv[1]);
     const string fileName = argv[2];
+    const int arity = atoi(argv[3]);
 
-    // Проверяем режим работы программы
-    switch (programMode) {
-        case 1:
-            cout << programMode << endl;
-            cout << fileName << endl;
-            break;
-        case 2:
-            cout << fileName << endl;
-            break;
-        default:
-            wcout << L"Режим программы может быть 1 или 2!" << endl;
-            return 0;
-    }
+    vector<Node*> tree;
+
+    tree = FileReader::readFile(fileName, arity);
+    FileReader::buildTreeConnections(tree, arity);
 
     return 0;
 }

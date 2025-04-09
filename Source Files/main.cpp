0660a520e5b6e9de4ee61d60956d5419ae8e5242
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-10 01:49:27 +0300

    Обновлена функция main для вызова всех необходимых вспомогательных функций

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <chrono>
 
-#include "../Header Files/Node.h"
+#include "../Header Files/FinderLongestOddPath.h"
+#include "../Header Files/FileGenerator.h"
 #include "../Header Files/FileReader.h"
 
 using namespace std;
@@ -19,6 +21,49 @@ int main(const int argc, char *argv[]) {
         wcout << L"nodeCount - количество узлов в n-арном дерева (задаётся, если programMode равен 2)" << endl;
         return 1;
     }
+
+    const int programMode = atoi(argv[1]);
+    const string fileName = argv[2];
+    // Если режим программы - 2, то генерируем файл
+    if (programMode == 2) {
+        int arity = atoi(argv[3]);
+        int nodeCount = atoi(argv[4]);
+        FileGenerator::generateTreeFile(fileName, arity, nodeCount);
+    }
+
+    // Читаем дерево из файла и устанавливаем связи между узлами
+    vector<Node*> tree = FileReader::readFile(argv[2]);
+    FileReader::buildTreeConnections(tree, tree[0]->getArity());
+
+    // Находим самый длинный путь от корня с нечётными значениями
+    vector<int> currentPath;
+    vector<int> longestPath;
+    // Замеряем время поиска в мксек
+    auto start = chrono::steady_clock::now();
+    FinderLongestOddPath::findLongestOddPath(tree[0], currentPath, longestPath);
+    auto end = chrono::steady_clock::now();
+
+    // Результат замеров
+    long duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+
+    // Выводим найденный путь
+    if (longestPath.size() < 2) {
+        wcout << L"Самый длинный путь от корня с нечётными вершинами не найден" << endl;
+    } else {
+        wcout << L"Самый длинный путь от корня, проходящий только по нечётным вершинам" << endl;
+        for (int i = 0; i < longestPath.size(); i++) {
+            cout << longestPath[i];
+            if (i < longestPath.size() - 1) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+        wcout << L"Длина пути: " << longestPath.size() - 1 << endl;
+        wcout << L"Время поиска путей: " << duration << L" мксек" << endl;
+    }
+
+    // Очищаем память
+    FileReader::cleanUpTree(tree);
 
     return 0;
 }

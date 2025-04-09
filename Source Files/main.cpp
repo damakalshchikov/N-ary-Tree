18daaba41c60fdce4d91f152ca144c198ce20e73
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-09 23:47:39 +0300

    Начальная структура mian

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -15,20 +15,10 @@ int main(const int argc, char *argv[]) {
         wcout << L"programMode - режим работы программы ";
         wcout << L"(1 - чтение существующего файла, 2 - генерация нового файла и его чтение)" << endl;
         wcout << L"fileName - имя файла для чтения" << endl;
-        wcout << L"arity - арность дерева" << endl;
+        wcout << L"arity - арность дерева (задаётся, если programMode равен 2)" << endl;
+        wcout << L"nodeCount - количество узлов в n-арном дерева (задаётся, если programMode равен 2)" << endl;
         return 1;
     }
-
-    // progMode - режим работы программы
-    // fileName - имя файла
-    const int programMode = atoi(argv[1]);
-    const string fileName = argv[2];
-    const int arity = atoi(argv[3]);
-
-    vector<Node*> tree;
-
-    tree = FileReader::readFile(fileName, arity);
-    FileReader::buildTreeConnections(tree, arity);
 
     return 0;
 }
